8c5fe07145292e0add0f9436b366d2b74236f89c
Илья Афанасьев <iafanasevmail@gmail.com>
2024-02-16 23:01:50 +0300

    added organizations serializers

---
+++ organizations/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from organizations.models import Organization
+from users.serializers import Empllo
+
+
+class OrganizationEventSerializer(serializers.ModelSerializer):
+    # employee = EmployeeSerializer(read_only=True, many=True)
+    organizations_details = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Organization
+        fields = ('title', 'organizations_details', 'employee')
+
+    def get_organizations_details(self, obj):
+        return f'{obj.postcode}, {obj.address}'
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = (
+            'title',
+            'description',
+            'address',
+            'postcode',
+        )
